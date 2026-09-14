a7656f386750736cbebe02839c4ac83dfdcaa4cd
xob0t <32616886+xob0t@users.noreply.github.com>
2026-09-14 16:05:21 +0300

    refactor(core): make core the backend implementation

---
+++ backend/compat.go
@@ -0,0 +1,13 @@
+package backend
+
+import "app/core"
+
+type UploadBatchStart = core.UploadBatchStart
+type PreflightWarning = core.PreflightWarning
+type ThreadStatus = core.ThreadStatus
+type FileUploadResult = core.FileUploadResult
+type AlbumStatus = core.AlbumStatus
+type AlbumError = core.AlbumError
+type UploadReporter = core.UploadReporter
+type FilesDroppedEvent = core.FilesDroppedEvent
+type StartUploadEvent = core.StartUploadEvent
